38629731e428012218369a421709483827cf3848
mabdullahsoyturk <muhammetabdullah.soyturk@agu.edu.tr>
2020-12-11 17:15:59 +0300

    retrieve best 5 for all fund types

---
+++ fund.py
@@ -0,0 +1,12 @@
+class Fund:
+    def __init__(self, code, name, type, one_month, three_months, six_months, from_new_year, one_year, three_years, five_years):
+        self.code = code
+        self.name = name
+        self.type = type
+        self.one_month = one_month
+        self.three_months = three_months
+        self.six_months = six_months
+        self.from_new_year = from_new_year
+        self.one_year = one_year
+        self.three_years = three_years
+        self.five_years = five_years
--- parse.py
+++ parse.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import selenium.common.exceptions
 import time, datetime, platform
+from fund import Fund
 from utils import get_arguments
 
 fund_types = ["Altın", "Borçlanma Araçları", "Değişken", "Endeks", "Eurobond", "Hisse Senedi", "Katılım"]
@@ -22,24 +23,45 @@ def launch_browser(driver_path):
 def parse_fund_type(browser, fund_type, best_of=5):
     select = browser.find_element_by_id('MainContent_DropDownListFundTypeExplanation')
     for option in select.find_elements_by_tag_name('option'):
-        print(option.text)
-        print(fund_type)
         if option.text == fund_type:
             option.click()
             break
 
     browser.execute_script("__doPostBack('ctl00$MainContent$GridViewFundReturn','Sort$GETIRI1A')")
-    time.sleep(10)
+    time.sleep(3)
     browser.execute_script("__doPostBack('ctl00$MainContent$GridViewFundReturn','Sort$GETIRI1A')")
+    time.sleep(3)
+    fund_table = browser.find_element_by_id("MainContent_GridViewFundReturn")
+    rows = fund_table.find_elements(By.TAG_NAME,"tr")
+    fund_elements = rows[1:best_of + 1]
 
+    funds = []
     
+    for fund_element in fund_elements:
+        fund_descriptions = fund_element.find_elements_by_class_name("fund-grid-item")
+        fund_performances = fund_element.find_elements_by_class_name("fund-grid-numeric")
+
+        fund_code = fund_descriptions[0].text
+        fund_name = fund_descriptions[1].text
+        fund_type = fund_performances[2].text.strip()
+        one_month = fund_performances[0].text.strip()
+        three_months = fund_performances[1].text.strip()
+        six_months = fund_performances[2].text.strip()
+        from_new_year = fund_performances[3].text.strip()
+        one_year = fund_performances[4].text.strip()
+        three_years = fund_performances[5].text.strip()
+        five_years = fund_performances[6].text.strip()
+
+        fund = Fund(fund_code, fund_name, fund_type, one_month, three_months, six_months, from_new_year, one_year, three_years, five_years)
+        funds.append(fund)
+
+    return funds
 
 def parse_fund_types(browser):
     best_funds_all = {}
 
     for fon_type in fund_types:
         best_funds = parse_fund_type(browser, fon_type)
-        exit()
         best_funds_all[fon_type] = best_funds
 
     return best_funds_all
@@ -48,4 +70,9 @@ if __name__ == "__main__":
     args = get_arguments()
 
     browser = launch_browser(args.driver_path)
-    parse_fund_types(browser)
+    best_funds_all = parse_fund_types(browser)
+
+    for fund_type in fund_types:
+        best_funds = best_funds_all[fund_type]
+        for fund in best_funds:
+            print(fund.name)
